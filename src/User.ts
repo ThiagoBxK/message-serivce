dd48dc47faba5a956ac372b29a5941a325cd4dcc
Thiago <thiago.soareshp2005@gmail.com>
2024-09-08 13:05:01 -0300

    fixed notify function on user class

--- src/User.ts
+++ src/User.ts
@@ -1,14 +1,12 @@
 class User {
   id: number;
   name: string;
+  notify: (message: string) => void;
 
   constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
-  }
-
-  notify(message: string) {
-    console.log(message);
+    this.notify = (message: string) => console.log(message);
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -7,36 +7,8 @@ const userA = new User(1, "Sofia");
 const userB = new User(2, "Michaela");
 const userC = new User(2, "Parker");
 
-chatService.subscribe("Conversas 1", {
-  ...userA,
-  notify: userA.notify,
-});
-
-chatService.subscribe("Conversas 1", {
-  ...userB,
-  notify: userB.notify,
-});
-
-chatService.notify(
-  "Conversas 1",
-  {
-    ...userA,
-    notify: userA.notify,
-  },
-  "Olá"
-);
-
-chatService.unsubscribe("Conversas 1", {
-  ...userA,
-  notify: userA.notify,
-});
-
-chatService.subscribe("Conversas 2", {
-  ...userC,
-  notify: userC.notify,
-});
-
-chatService.subscribe("Conversas 2", {
-  ...userA,
-  notify: userA.notify,
-});
+chatService.subscribe("Conversas 1", userA);
+chatService.subscribe("Conversas 1", userB);
+chatService.unsubscribe("Conversas 1", userA);
+chatService.subscribe("Conversas 2", userC);
+chatService.subscribe("Conversas 2", userA);
